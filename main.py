e886e15683fc98a311ad1cccdfcd870c8f8e99d6
Gerard <gerardfinol@gmail.com>
2019-10-24 18:16:22 +0200

    add support for keyword changes

--- main.py
+++ main.py
@@ -24,39 +24,48 @@ class JetbrainsLauncherExtension(Extension):
         """ Initializes the extension """
         super(JetbrainsLauncherExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-        ides = ['pstorm', 'webstorm', 'pycharm', 'intellij']
-        self.projects_file_switcher = {self.preferences.get(f"{ide}_keyword"): self.preferences.get(f"{ide}_projects_file")
-                                       for ide in ides}
-        self.launch_script_switcher = {self.preferences.get(f"{ide}_keyword"): self.preferences.get(f"{ide}_launch_script")
-                                       for ide in ides}
-
-    def get_recent_projects_file_path(self, keyword: str):
-        """ Returns the file path where the recent projects are stored """
-        return os.path.expanduser(self.projects_file_switcher.get(keyword, ""))
 
     @staticmethod
-    def get_icon(keyword):
+    def get_recent_projects_file_path(keyword: str, projects_file_switcher):
+        """ Returns the file path where the recent projects are stored """
+        logging.info(f"keyword {keyword}")
+        return os.path.expanduser(projects_file_switcher.get(keyword, ""))
+
+    @staticmethod
+    def get_icon(image_file):
         """ Returns the application icon based on the keyword """
-        icon_path = os.path.join('images', "%s.png" % keyword)
+        icon_path = os.path.join('images', image_file)
 
         return icon_path
 
-    def get_launcher_file(self, keyword):
+    @staticmethod
+    def get_launcher_file(keyword, launch_script_switcher):
         """ Returns the launcher file from preferences"""
-        return os.path.expanduser(self.launch_script_switcher.get(keyword))
+        return os.path.expanduser(launch_script_switcher.get(keyword))
 
 
 class KeywordQueryEventListener(EventListener):
     """ Listener that handles the user input """
 
-    @staticmethod
-    def on_event(event, extension):
+    def on_event(self, event, extension):
+        preferences = extension.preferences
+        ides = ['pstorm', 'webstorm', 'pycharm', 'intellij']
+        projects_file_switcher = {
+            preferences.get(f"{ide}_keyword"): preferences.get(f"{ide}_projects_file") for ide in ides}
+        launch_script_switcher = {
+            preferences.get(f"{ide}_keyword"): preferences.get(f"{ide}_launch_script") for ide in ides}
+        icon_switcher = {preferences.get(f"{ide}_keyword"): f"{ide}.png" for ide in ides}
+
+        logging.info(f"launch script: {launch_script_switcher}")
+        logging.info(f"projects files: {projects_file_switcher}")
+
         """ Handles the event """
         items = []
-
+        logging.info(f"preferences {extension.preferences}")
         keyword = event.get_keyword()
+        logging.info(f"keyword: {keyword}")
         query = event.get_argument() or ""
-        file_path = extension.get_recent_projects_file_path(keyword)
+        file_path = extension.get_recent_projects_file_path(keyword, projects_file_switcher)
 
         projects = RecentProjectsParser.parse(file_path, query)
 
@@ -70,13 +79,12 @@ class KeywordQueryEventListener(EventListener):
             ])
 
         for project in projects:
-
             items.append(ExtensionResultItem(
-                icon=extension.get_icon(keyword),
+                icon=extension.get_icon(icon_switcher[keyword]),
                 name=project['name'],
                 description=project['path'],
                 on_enter=RunScriptAction('%s "%s" &' % (
-                    extension.get_launcher_file(keyword), project['path']), []),
+                    extension.get_launcher_file(keyword, launch_script_switcher), project['path']), []),
                 on_alt_enter=CopyToClipboardAction(project['path'])
             ))
 
